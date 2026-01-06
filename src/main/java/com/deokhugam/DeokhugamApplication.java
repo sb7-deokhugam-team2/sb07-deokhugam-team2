a19dbd243d42b63a5709f5fdea284c1bbe079a0b
hilee <eldh2002@gmail.com>
2026-01-06 18:49:09 +0900

    initailize

---
+++ src/main/java/com/deokhugam/DeokhugamApplication.java
@@ -0,0 +1,13 @@
+package com.deokhugam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeokhugamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeokhugamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deokhugam/deokhugam/DeokhugamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deokhugam.deokhugam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeokhugamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
