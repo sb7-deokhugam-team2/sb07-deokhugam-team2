5bb994bc030f6b98549030c9e1cd43a806fa0746
hilee <eldh2002@gmail.com>
2026-01-07 16:23:34 +0900

    add : baseEntity 생성 / dto reponse,request package path 추가

---
+++ src/main/java/com/deokhugam/domain/base/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.deokhugam.domain.base;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false)
+    private UUID id;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
