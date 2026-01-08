67d59137a080e7c4dee5a37fc2c9391ecf757ca7
hilee <eldh2002@gmail.com>
2026-01-08 15:46:20 +0900

    fix: 값에 제약조건 추가 / timestamp를 timestamp with time zone으로 변경 / default 셋팅 변경

--- src/main/java/com/deokhugam/DeokhugamApplication.java
+++ src/main/java/com/deokhugam/DeokhugamApplication.java
@@ -3,10 +3,13 @@ package com.deokhugam;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class DeokhugamApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(DeokhugamApplication.class, args);
     }
 
