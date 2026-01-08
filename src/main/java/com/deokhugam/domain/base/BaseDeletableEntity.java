10102a6c80c395eff80d0a5cbc13c7b2fc219106
hilee <eldh2002@gmail.com>
2026-01-08 15:48:24 +0900

    fix: isDeleted에 default값 추가, @Column 추가

--- src/main/java/com/deokhugam/domain/base/BaseDeletableEntity.java
+++ src/main/java/com/deokhugam/domain/base/BaseDeletableEntity.java
@@ -1,12 +1,15 @@
 package com.deokhugam.domain.base;
 
+ㅇimport jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import org.hibernate.annotations.ColumnDefault;
 
 @Getter
 @MappedSuperclass
 public abstract class BaseDeletableEntity extends BaseUpdateEntity {
-    private boolean isDeleted;
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted = false;
 
     public void delete() {
         isDeleted = true;
