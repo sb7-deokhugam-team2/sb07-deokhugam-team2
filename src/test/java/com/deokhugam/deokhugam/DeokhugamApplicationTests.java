813ccb0f80dc2c29fe15c7c35439d958e54adbc8
hilee <eldh2002@gmail.com>
2026-01-08 17:20:41 +0900

    test

--- src/test/java/com/deokhugam/deokhugam/DeokhugamApplicationTests.java
+++ src/test/java/com/deokhugam/deokhugam/DeokhugamApplicationTests.java
@@ -3,11 +3,14 @@ package com.deokhugam.deokhugam;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class DeokhugamApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue(false);
     }
 
 }
