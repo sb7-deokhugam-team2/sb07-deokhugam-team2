e0240cdf3e832df47a1e1beceadad1fca4cc84e7
ian <dldks1290@gmail.com>
2026-01-09 14:31:51 +0900

    feat: 조회, 논리삭제, 물리삭제 메서드 구현

--- src/main/java/com/deokhugam/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/deokhugam/domain/user/service/UserServiceImpl.java
@@ -4,9 +4,13 @@ import com.deokhugam.domain.user.dto.request.UserLoginRequest;
 import com.deokhugam.domain.user.dto.request.UserRegisterRequest;
 import com.deokhugam.domain.user.dto.request.UserUpdateRequest;
 import com.deokhugam.domain.user.dto.response.UserDto;
+import com.deokhugam.domain.user.entity.User;
+import com.deokhugam.domain.user.exception.UserNotFoundException;
 import com.deokhugam.domain.user.repository.UserRepository;
+import com.deokhugam.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
@@ -28,21 +32,32 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public UserDto findUser(UUID userId) {
-        return null;
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException(ErrorCode.USER_NOT_FOUND));
+        return UserDto.from(user);
     }
 
     @Override
+    @Transactional
     public void logicalDelete(UUID userId) {
-
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException(ErrorCode.USER_NOT_FOUND));
+        user.delete();
     }
 
     @Override
-    public UserDto updateNickname(UserUpdateRequest userUpdateRequest) {
-        return null;
+    @Transactional
+    public UserDto updateNickname(UUID userId, UserUpdateRequest userUpdateRequest) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException(ErrorCode.USER_NOT_FOUND));
+        user.updateNickname(userUpdateRequest.nickname());
+        return UserDto.from(user);
     }
 
     @Override
     public void physicalDelete(UUID userId) {
-
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException(ErrorCode.USER_NOT_FOUND));
+        userRepository.delete(user);
     }
 }
