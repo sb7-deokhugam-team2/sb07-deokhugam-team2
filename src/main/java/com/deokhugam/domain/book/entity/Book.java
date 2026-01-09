7a398a59ff91442c9d459a93847596dc3e39275e
taeeonkim <myhom43@gmail.com>
2026-01-09 12:28:03 +0900

    feat: BookRepository JPA 슬라이스 테스트 추가 및 엔티티 생성자 추가

--- src/main/java/com/deokhugam/domain/book/entity/Book.java
+++ src/main/java/com/deokhugam/domain/book/entity/Book.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 @Entity
 @Getter
 @Table(name = "books")
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Book extends BaseDeletableEntity {
     @Column(name = "title", nullable = false)
