73ebda21a61f47a0f515f96dd8876846977318b2
JoSungYeon-000607 <zounsam00@gmail.com>
2026-01-09 16:25:37 +0900

    hotfix : 기존 Long으로 되어있던 nextCursor 자료형 Instant로 수정

--- src/main/java/com/deokhugam/domain/book/dto/response/CursorPageResponseBookDto.java
+++ src/main/java/com/deokhugam/domain/book/dto/response/CursorPageResponseBookDto.java
@@ -1,11 +1,12 @@
 package com.deokhugam.domain.book.dto.response;
 
+import java.time.Instant;
 import java.util.List;
 
 public record CursorPageResponseBookDto(
         List<BookDto> content,
         String nextCursor,
-        Long nextAfter,
+        Instant nextAfter,
         Integer size,
         Long totalElements,
         boolean hasNext
