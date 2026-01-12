c48e4b3b679b229a8da0f269130190024dfeb749
hilee <eldh2002@gmail.com>
2026-01-12 18:39:35 +0900

    feat: Review 목록 조회 관련 interface 및 DTO 추가

---
+++ src/main/java/com/deokhugam/domain/review/repository/ReviewRepositoryCustom.java
@@ -0,0 +1,11 @@
+package com.deokhugam.domain.review.repository;
+
+import com.deokhugam.domain.review.dto.request.CursorPageRequest;
+import com.deokhugam.domain.review.dto.request.ReviewSearchCondition;
+import com.deokhugam.domain.review.dto.response.ReviewPageResponseDto;
+
+import java.util.UUID;
+
+public interface ReviewRepositoryCustom {
+    ReviewPageResponseDto search(ReviewSearchCondition condition, CursorPageRequest pageRequest, UUID requestId);
+}
