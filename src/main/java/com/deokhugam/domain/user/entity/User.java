91dce815b39d1c181a75a3ac8abfb9116ae66344
ian <dldks1290@gmail.com>
2026-01-12 13:44:37 +0900

    feat: User 생성을 정적 메서드로 변경, 검증 로직 추가

--- src/main/java/com/deokhugam/domain/user/entity/User.java
+++ src/main/java/com/deokhugam/domain/user/entity/User.java
@@ -1,18 +1,18 @@
 package com.deokhugam.domain.user.entity;
 
 import com.deokhugam.domain.base.BaseDeletableEntity;
+import com.deokhugam.domain.user.exception.UserPasswordValidationException;
+import com.deokhugam.global.exception.ErrorCode;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @Table(name = "users")
-@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseDeletableEntity {
     @Column(name = "email", nullable = false, unique = true)
@@ -24,9 +24,43 @@ public class User extends BaseDeletableEntity {
     @Column(name = "password", nullable = false)
     private String password;
 
-    public void updateNickname(String newNickname){
-        if(newNickname!=null){
-            this.nickname=newNickname;
+    private User(String email, String nickname, String password) {
+        this.email = email;
+        this.nickname = nickname;
+        this.password = password;
+    }
+
+    public static User create(String email, String nickname, String password) {
+        validateEmail(email);
+        validateNickname(nickname);
+        validatePassword(password);
+        return new User(email, nickname, password);
+    }
+
+    public void updateNickname(String newNickname) {
+        validateNickname(newNickname);
+        this.nickname = newNickname;
+    }
+
+    private static void validatePassword(String password) {
+        if (password != null) {
+            String pwdRegex = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,20}$";
+            if (password.matches(pwdRegex)) {
+                return;
+            }
+        }
+        throw new UserPasswordValidationException(ErrorCode.USER_PASSWORD_VALIDATION);
+    }
+
+    private static void validateNickname(String nickname) {
+        if (nickname == null || nickname.length() < 2) {
+            throw new UserPasswordValidationException(ErrorCode.USER_NICKNAME_VALIDATION);
+        }
+    }
+
+    private static void validateEmail(String email) {
+        if (!email.contains("@")) {
+            throw new UserPasswordValidationException(ErrorCode.USER_EMAIL_VALIDATION);
         }
     }
 }
