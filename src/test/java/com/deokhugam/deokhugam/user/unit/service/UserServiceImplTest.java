e0396af1d4fd501465125600dc58d6b5aafc7621
yumi Kim <cdcd0214@gmail.com>
2026-01-12 11:01:49 +0900

    feat : UserServiceImplTest에 register, login 테스트 추가

--- src/test/java/com/deokhugam/deokhugam/user/unit/service/UserServiceImplTest.java
+++ src/test/java/com/deokhugam/deokhugam/user/unit/service/UserServiceImplTest.java
@@ -1,12 +1,18 @@
 package com.deokhugam.deokhugam.user.unit.service;
 
+import com.deokhugam.domain.user.dto.request.UserLoginRequest;
+import com.deokhugam.domain.user.dto.request.UserRegisterRequest;
 import com.deokhugam.domain.user.dto.request.UserUpdateRequest;
 import com.deokhugam.domain.user.dto.response.UserDto;
 import com.deokhugam.domain.user.entity.User;
+import com.deokhugam.domain.user.exception.UserAlreadyExistsException;
+import com.deokhugam.domain.user.exception.UserEmailNotExistsException;
 import com.deokhugam.domain.user.exception.UserNotFoundException;
+import com.deokhugam.domain.user.exception.UserPasswordException;
 import com.deokhugam.domain.user.repository.UserRepository;
 import com.deokhugam.domain.user.service.UserService;
 import com.deokhugam.domain.user.service.UserServiceImpl;
+import com.deokhugam.global.exception.ErrorCode;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,6 +24,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -30,28 +37,99 @@ public class UserServiceImplTest {
     @InjectMocks
     UserServiceImpl userService;
 
-//    @Test
-//    void register(){
-//        //given
-//
-//        //when
-//
-//        //then
-//
-//    }
+    @Test
+    @DisplayName("회원가입 성공 테스트")
+    void register_success() {
+        //given
+        User user = new User("test@gmail.com", "testName", "1234");
+        UserRegisterRequest userRegisterRequest = new UserRegisterRequest(user.getEmail(), user.getNickname(), user.getPassword());
 
-//    @Test
-//    void login(){
-//        //given
-//
-//        //when
-//
-//        //then
-//
-//    }
+        lenient().when(userRepository.existsByEmail(user.getEmail())).thenReturn(false);
+        lenient().when(userRepository.save(any(User.class))).thenReturn(user);
+
+        //when
+        UserDto userDto = userService.register(userRegisterRequest);
+
+        //then
+        assertThat(userDto.getEmail()).isEqualTo(user.getEmail());
+        assertThat(userDto.getNickname()).isEqualTo(user.getNickname());
+
+        verify(userRepository, times(1)).save(any(User.class));
+
+    }
 
     @Test
-    void findUser(){
+    @DisplayName("회원가입 실패 테스트 : 이미 존재하는 이메일일 경우 UserAlreadyExistsException 실행")
+    void register_fail() {
+        // given
+        UserRegisterRequest userRegisterRequest = new UserRegisterRequest(
+                "duplication@test.com", "testName", "1234");
+
+        // when & then
+        when(userRepository.existsByEmail(userRegisterRequest.email())).thenReturn(true);
+
+        assertThrows(UserAlreadyExistsException.class, () -> {
+            userService.register(userRegisterRequest);
+        });
+
+        verify(userRepository, never()).save(any(User.class));
+    }
+
+    @Test
+    @DisplayName("로그인 성공 테스트")
+    void login_success() {
+        //given
+        String email = "test@gamil";
+        String password = "1234";
+        UserLoginRequest userLoginRequest = new UserLoginRequest(email, password);
+
+        User user = new User(email, "nickName", password);
+        when(userRepository.findByEmail(email)).thenReturn(Optional.of(user));
+        //when
+        UserDto userDto = userService.login(userLoginRequest);
+
+        //then
+        assertThat(userDto.getEmail()).isEqualTo(email);
+        verify(userRepository, times(1)).findByEmail(email);
+    }
+
+    @Test
+    @DisplayName("로그인 실패 테스트 : 1. 이메일이 틀릴 경우 UserEmailNotExistsException 실행")
+    void login_fail_email() {
+        // given
+        String email = "test@gmail.com";
+        UserLoginRequest userLoginRequest = new UserLoginRequest(email, "1234");
+
+        when(userRepository.findByEmail(email)).thenReturn(Optional.empty());
+
+        // when & then
+        UserEmailNotExistsException exception = assertThrows(UserEmailNotExistsException.class, () -> {
+            userService.login(userLoginRequest);
+        });
+        assertThat(exception.getErrorCode()).isEqualTo(ErrorCode.USER_EMAIL_NOT_EXISTS);
+    }
+
+    @Test
+    @DisplayName("로그인 실패 테스트 : 2. 비밀번호가 일치하지 않을 경우 UserPasswordException 실행")
+    void login_fail_password() {
+        // given
+        String email = "test@gamil";
+        UserLoginRequest userLoginRequest = new UserLoginRequest(email, "1234");
+
+        User user = new User(email, "nickName", "0000");
+
+        when(userRepository.findByEmail(email)).thenReturn(Optional.of(user));
+
+        // when & then
+        UserPasswordException exception = assertThrows(UserPasswordException.class, () -> {
+            userService.login(userLoginRequest);
+        });
+
+        assertThat(exception.getErrorCode()).isEqualTo(ErrorCode.USER_PASSWORD_NOT_EQUAL);
+    }
+
+    @Test
+    void findUser() {
         //given
         User user = new User("test@gmail.com", "testName", "1234");
         when(userRepository.findById(any(UUID.class)))
@@ -67,7 +145,7 @@ public class UserServiceImplTest {
 
     @Test
     @DisplayName("논리 삭제 성공")
-    void logicalDelete(){
+    void logicalDelete() {
         //given
         User user = new User("test@gmail.com", "testName", "1234");
         when(userRepository.findById(any(UUID.class)))
@@ -83,7 +161,7 @@ public class UserServiceImplTest {
 
     @Test
     @DisplayName("논리 삭제 실패: 유저가 없으면 UserNotFoundException 발생")
-    void logicalDelete_not_foud(){
+    void logicalDelete_not_foud() {
         //given
         when(userRepository.findById(any(UUID.class)))
                 .thenReturn(Optional.empty());
@@ -98,7 +176,7 @@ public class UserServiceImplTest {
 
     @Test
     @DisplayName("수정 성공")
-    void updateNickname(){
+    void updateNickname() {
         //given
         UserUpdateRequest userUpdateRequest = new UserUpdateRequest("newName");
         User user = new User("test@gmail.com", "testName", "1234");
@@ -127,7 +205,7 @@ public class UserServiceImplTest {
 
     @Test
     @DisplayName("물리 삭제 성공")
-    void physicalDelete(){
+    void physicalDelete() {
         //given
         User user = new User("test@gmail.com", "testName", "1234");
         when(userRepository.findById(any(UUID.class)))
