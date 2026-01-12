d3a95f5878d86997b1019c70dd09514e1033a5a8
ian <dldks1290@gmail.com>
2026-01-12 11:45:48 +0900

    feat: UserRepositoryTest 작성

--- src/test/java/com/deokhugam/deokhugam/user/integration/slice/jpa/UserRepositoryTest.java
+++ src/test/java/com/deokhugam/deokhugam/user/integration/slice/jpa/UserRepositoryTest.java
@@ -1,12 +1,18 @@
 package com.deokhugam.deokhugam.user.integration.slice.jpa;
 
+import com.deokhugam.domain.user.entity.User;
 import com.deokhugam.domain.user.repository.UserRepository;
 import com.deokhugam.global.config.JpaAuditingConfig;
 import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
 
 @DataJpaTest
 @Import(JpaAuditingConfig.class)
@@ -18,13 +24,30 @@ public class UserRepositoryTest {
     @Autowired
     EntityManager em;
 
-//    @Test
-//    void existsByEmail(){
-//
-//    }
-//
-//    @Test
-//    void findByEmail(){
-//
-//    }
+    @Test
+    void existsByEmail(){
+        //given
+        User user = new User("test@gmail.com", "nickname", "1234");
+        userRepository.save(user);
+
+        //when
+        boolean result = userRepository.existsByEmail("test@gmail.com");
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void findByEmail(){
+        //given
+        User user = new User("test@gmail.com", "nickname", "1234");
+        userRepository.save(user);
+
+        //when
+        User findUser = userRepository.findByEmail("test@gmail.com").orElseThrow(RuntimeException::new);
+
+        //then
+        assertThat(findUser).isEqualTo(user);
+
+    }
 }
