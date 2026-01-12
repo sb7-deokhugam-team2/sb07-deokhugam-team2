a574dd2cac27a4df5ec2d3aee0968c42b3df7c2b
ian <dldks1290@gmail.com>
2026-01-12 13:45:51 +0900

    feat: 컨트롤러 테스트 추가, 리포지토리 테스트 완성

--- src/test/java/com/deokhugam/deokhugam/user/integration/slice/jpa/UserRepositoryTest.java
+++ src/test/java/com/deokhugam/deokhugam/user/integration/slice/jpa/UserRepositoryTest.java
@@ -27,7 +27,7 @@ public class UserRepositoryTest {
     @Test
     void existsByEmail(){
         //given
-        User user = new User("test@gmail.com", "nickname", "1234");
+        User user = User.create("test@gmail.com", "nickname", "1234");
         userRepository.save(user);
 
         //when
@@ -40,7 +40,7 @@ public class UserRepositoryTest {
     @Test
     void findByEmail(){
         //given
-        User user = new User("test@gmail.com", "nickname", "1234");
+        User user = User.create("test@gmail.com", "nickname", "1234");
         userRepository.save(user);
 
         //when
