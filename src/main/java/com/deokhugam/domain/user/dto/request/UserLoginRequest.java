e4890bf7dc8925ea5dc024d07613a0a3b7382b48
ian <dldks1290@gmail.com>
2026-01-12 13:45:28 +0900

    fix: User.create() 사용, request에 검증 애너테이션 추가

--- src/main/java/com/deokhugam/domain/user/dto/request/UserLoginRequest.java
+++ src/main/java/com/deokhugam/domain/user/dto/request/UserLoginRequest.java
@@ -1,6 +1,11 @@
 package com.deokhugam.domain.user.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record UserLoginRequest(
+        @NotBlank
         String email,
+        @NotBlank
         String password
-) {}
+) {
+}
