cdf5d39fbcf0ef8999bcae66af38d359aadcd1a8
hilee <eldh2002@gmail.com>
2026-01-13 13:31:52 +0900

    feat: Controller 기본 틀 작성 및 Request DTO에 Validation 적용

--- src/main/java/com/deokhugam/domain/review/dto/request/ReviewUpdateRequest.java
+++ src/main/java/com/deokhugam/domain/review/dto/request/ReviewUpdateRequest.java
@@ -1,7 +1,15 @@
 package com.deokhugam.domain.review.dto.request;
 
+import jakarta.validation.constraints.*;
+
 public record ReviewUpdateRequest(
+        @NotNull
+        @DecimalMax("5.0")
+        @DecimalMin(value = "0.0", inclusive = false)
         Double rating,
+
+        @NotBlank
+        @Size(max = 500)
         String content
 ) {
 }
