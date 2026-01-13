66463715180beb116ef705a5b77de5130a5ed9f6
ian <dldks1290@gmail.com>
2026-01-13 10:31:02 +0900

    feat: 예외 작성

--- src/main/java/com/deokhugam/global/exception/ErrorCode.java
+++ src/main/java/com/deokhugam/global/exception/ErrorCode.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
     // User
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다.", "U-001"),
+    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "요청이 올바르지 않습니다.", "U-001"),
     USER_EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "이메일 중복", "U-002"),
     USER_EMAIL_NOT_EXISTS(HttpStatus.UNAUTHORIZED, "이메일이 존재하지 않습니다.", "U-003"),
     USER_PASSWORD_NOT_EQUAL(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다.", "U-004"),
