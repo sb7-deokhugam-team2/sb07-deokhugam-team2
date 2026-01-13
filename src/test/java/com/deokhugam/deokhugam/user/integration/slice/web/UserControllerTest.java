680efbbd7c842d586a6d4a2ffd3146057a0fc634
yumi Kim <cdcd0214@gmail.com>
2026-01-13 13:20:06 +0900

    feat : UserController register, login 작성, UserControllerTest register 작성

--- src/test/java/com/deokhugam/deokhugam/user/integration/slice/web/UserControllerTest.java
+++ src/test/java/com/deokhugam/deokhugam/user/integration/slice/web/UserControllerTest.java
@@ -1,15 +1,20 @@
 package com.deokhugam.deokhugam.user.integration.slice.web;
 
 import com.deokhugam.domain.user.controller.UserController;
+import com.deokhugam.domain.user.dto.request.UserRegisterRequest;
 import com.deokhugam.domain.user.dto.request.UserUpdateRequest;
 import com.deokhugam.domain.user.dto.response.UserDto;
 import com.deokhugam.domain.user.entity.User;
+import com.deokhugam.domain.user.exception.UserAlreadyExistsException;
 import com.deokhugam.domain.user.service.UserServiceImpl;
+import com.deokhugam.global.exception.ErrorCode;
+import com.deokhugam.global.exception.GlobalExceptionHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -23,7 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(UserController.class)
+@WebMvcTest({UserController.class, GlobalExceptionHandler.class})
 public class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -32,19 +37,47 @@ public class UserControllerTest {
     @MockitoBean
     UserServiceImpl userService;
 
-//    @Test
-//    void register(){
-//        //given
-//
-//        //when&then
-//    }
+    @Test
+    @DisplayName("유저 가입 성공")
+    void register_success() throws Exception {
+        //given : 특정 동작 정의
+        UserRegisterRequest userRegisterRequest = new UserRegisterRequest(
+                "test@gmail.com", "test123", "12345678a!"
+        );
 
-//    @Test
-//    void login(){
-//        //given
-//
-//        //when&then
-//    }
+        //when&then : 요청 수행, 결과 검증
+        mockMvc.perform(post("/api/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userRegisterRequest)))
+                .andExpect(status().isCreated());
+
+        verify(userService).register(any(UserRegisterRequest.class));
+    }
+
+    @Test
+    @DisplayName("유저 가입 실패 : 이메일 중복")
+    void register_fail() throws Exception {
+    // given
+    UserRegisterRequest userRegisterRequest = new UserRegisterRequest(
+            "test@gmail.com", "test123", "12345678a!"
+    );
+    when(userService.register(any())).thenThrow(new UserAlreadyExistsException(ErrorCode.USER_EMAIL_ALREADY_EXISTS));
+
+    // when&then
+    mockMvc.perform(post("/api/users")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(userRegisterRequest)))
+            .andExpect(status().isConflict());
+
+    verify(userService).register(any(UserRegisterRequest.class));
+    }
+
+    @Test
+    void login() {
+        //given
+
+        //when&then
+    }
 
     @Test
     @DisplayName("유저 조회 성공")
