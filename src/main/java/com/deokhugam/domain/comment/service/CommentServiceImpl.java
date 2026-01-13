1fcad016c41e0a07915b0a9c887f465c9a1f5e47
ian <dldks1290@gmail.com>
2026-01-13 10:31:25 +0900

    feat: CommentServiceImpl 조회, 삭제, 수정 메서드 작성

--- src/main/java/com/deokhugam/domain/comment/service/CommentServiceImpl.java
+++ src/main/java/com/deokhugam/domain/comment/service/CommentServiceImpl.java
@@ -5,14 +5,23 @@ import com.deokhugam.domain.comment.dto.request.CommentSearchCondition;
 import com.deokhugam.domain.comment.dto.request.CommentUpdateRequest;
 import com.deokhugam.domain.comment.dto.response.CommentDto;
 import com.deokhugam.domain.comment.dto.response.CursorPageResponseCommentDto;
+import com.deokhugam.domain.comment.entity.Comment;
+import com.deokhugam.domain.comment.exception.CommentNotFound;
+import com.deokhugam.domain.comment.exception.CommentUnauthorizedException;
+import com.deokhugam.domain.comment.repository.CommentRepository;
+import com.deokhugam.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class CommentServiceImpl implements CommentService{
+
+    private final CommentRepository commentRepository;
+
     @Override
     public CursorPageResponseCommentDto findContents(CommentSearchCondition commentSearchCondition) {
         return null;
@@ -24,22 +33,45 @@ public class CommentServiceImpl implements CommentService{
     }
 
     @Override
+    @Transactional(readOnly = true)
     public CommentDto findComment(UUID commentId) {
-        return null;
+        return commentRepository.findCommentDto(commentId).orElseThrow(() -> new CommentNotFound(ErrorCode.COMMENT_NOT_FOUND));
     }
 
     @Override
+    @Transactional
     public void logicalDelete(UUID commentId, UUID userId) {
+        Comment comment = commentRepository.findWithUser(commentId).orElseThrow(() -> new CommentNotFound(ErrorCode.COMMENT_NOT_FOUND));
 
+        if(!comment.isAuthor(userId)){
+            throw new CommentUnauthorizedException(ErrorCode.COMMENT_UNAUTHORIZED);
+        }
+
+        comment.delete();
     }
 
     @Override
+    @Transactional
     public CommentDto updateComment(UUID commentId, UUID userId, CommentUpdateRequest commentUpdateRequest) {
-        return null;
+        Comment comment = commentRepository.findWithUserAndReview(commentId).orElseThrow(() -> new CommentNotFound(ErrorCode.COMMENT_NOT_FOUND));
+
+        if(!comment.isAuthor(userId)){
+            throw new CommentUnauthorizedException(ErrorCode.COMMENT_UNAUTHORIZED);
+        }
+
+        comment.updateContent(commentUpdateRequest.content());
+        return CommentDto.from(comment);
     }
 
     @Override
+    @Transactional
     public void physicalDelete(UUID commentId, UUID userId) {
+        Comment comment = commentRepository.findWithUser(commentId).orElseThrow(() -> new CommentNotFound(ErrorCode.COMMENT_NOT_FOUND));
 
+        if(!comment.isAuthor(userId)){
+            throw new CommentUnauthorizedException(ErrorCode.COMMENT_UNAUTHORIZED);
+        }
+
+        commentRepository.delete(comment);
     }
 }
