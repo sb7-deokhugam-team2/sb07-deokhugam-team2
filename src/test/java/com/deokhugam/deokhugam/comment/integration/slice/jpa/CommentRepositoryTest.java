7ab1843d48849baa90b9b088100d69c61bef6fc4
ian <dldks1290@gmail.com>
2026-01-13 17:28:52 +0900

    feat: CommentRepositoryTest @Displayname 추가

--- src/test/java/com/deokhugam/deokhugam/comment/integration/slice/jpa/CommentRepositoryTest.java
+++ src/test/java/com/deokhugam/deokhugam/comment/integration/slice/jpa/CommentRepositoryTest.java
@@ -14,6 +14,8 @@ import com.deokhugam.global.config.JpaAuditingConfig;
 import com.deokhugam.global.config.QuerydslConfig;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -41,70 +43,81 @@ public class CommentRepositoryTest {
 
     @Autowired
     UserRepository userRepository;
+
     @Autowired
     ReviewRepository reviewRepository;
+
     @Autowired
     BookRepository bookRepository;
 
 
-    @Test
-    void findWithUser() {
-        //given
-        User user = User.create("test@gmail", "test", "12345678q!");
-        User savedUser = userRepository.save(user);
-        Book book = Book.create(
-                "title", "content", "12345678",
-                LocalDate.now(), "publisher",
-                "thumbnailUrl", "description");
-        Book savedBook = bookRepository.save(book);
-        Review review = Review.create(5.0, "content", savedBook, savedUser);
-        Review savedReview = reviewRepository.save(review);
-        Comment comment = Comment.create("content", savedUser, savedReview);
-        Comment savedComment = commentRepository.save(comment);
-        em.flush();
-        em.clear();
+    @Nested
+    @DisplayName("FetchJoin 메서드")
+    class FetchJoin{
+        @Test
+        @DisplayName("Comment 조회 시 user를 페치 조인")
+        void findWithUser() {
+            //given
+            User user = User.create("test@gmail", "test", "12345678q!");
+            User savedUser = userRepository.save(user);
+            Book book = Book.create(
+                    "title", "content", "12345678",
+                    LocalDate.now(), "publisher",
+                    "thumbnailUrl", "description");
+            Book savedBook = bookRepository.save(book);
+            Review review = Review.create(5.0, "content", savedBook, savedUser);
+            Review savedReview = reviewRepository.save(review);
+            Comment comment = Comment.create("content", savedUser, savedReview);
+            Comment savedComment = commentRepository.save(comment);
+            em.flush();
+            em.clear();
 
-        //when
-        Comment findComment = commentRepository.findWithUser(savedComment.getId()).orElseThrow();
+            //when
+            Comment findComment = commentRepository.findWithUser(savedComment.getId()).orElseThrow();
 
-        //then
-        boolean userIsLoaded = emf.getPersistenceUnitUtil().isLoaded(findComment.getUser());
-        assertThat(userIsLoaded).isTrue();
-        assertThat(savedComment.getContent()).isEqualTo(comment.getContent());
-        assertThat(savedComment.getUser().getNickname()).isEqualTo(user.getNickname());
+            //then
+            boolean userIsLoaded = emf.getPersistenceUnitUtil().isLoaded(findComment.getUser());
+            assertThat(userIsLoaded).isTrue();
+            assertThat(findComment.getContent()).isEqualTo(comment.getContent());
+            assertThat(findComment.getUser().getNickname()).isEqualTo(user.getNickname());
+        }
+
+        @Test
+        @DisplayName("Comment 조회 시 user와 review를 페치 조인")
+        void findWithUserAndReview() {
+            //given
+            User user = User.create("test@gmail", "test", "12345678q!");
+            User savedUser = userRepository.save(user);
+            Book book = Book.create(
+                    "title", "content", "12345678",
+                    LocalDate.now(), "publisher",
+                    "thumbnailUrl", "description");
+            Book savedBook = bookRepository.save(book);
+            Review review = Review.create(5.0, "content", savedBook, savedUser);
+            Review savedReview = reviewRepository.save(review);
+            Comment comment = Comment.create("content", savedUser, savedReview);
+            Comment savedComment = commentRepository.save(comment);
+            em.flush();
+            em.clear();
+
+            //when
+            Comment findComment = commentRepository.findWithUserAndReview(savedComment.getId()).orElseThrow();
+
+            //then
+            boolean userIsLoaded = emf.getPersistenceUnitUtil().isLoaded(findComment.getUser());
+            boolean reviewIsLoaded = emf.getPersistenceUnitUtil().isLoaded(findComment.getReview());
+            assertThat(userIsLoaded).isTrue();
+            assertThat(reviewIsLoaded).isTrue();
+            assertThat(findComment.getContent()).isEqualTo(comment.getContent());
+            assertThat(findComment.getUser().getNickname()).isEqualTo(user.getNickname());
+            assertThat(findComment.getReview().getContent()).isEqualTo(review.getContent());
+        }
     }
 
-    @Test
-    void findWithUserAndReview() {
-        //given
-        User user = User.create("test@gmail", "test", "12345678q!");
-        User savedUser = userRepository.save(user);
-        Book book = Book.create(
-                "title", "content", "12345678",
-                LocalDate.now(), "publisher",
-                "thumbnailUrl", "description");
-        Book savedBook = bookRepository.save(book);
-        Review review = Review.create(5.0, "content", savedBook, savedUser);
-        Review savedReview = reviewRepository.save(review);
-        Comment comment = Comment.create("content", savedUser, savedReview);
-        Comment savedComment = commentRepository.save(comment);
-        em.flush();
-        em.clear();
 
-        //when
-        Comment findComment = commentRepository.findWithUserAndReview(savedComment.getId()).orElseThrow();
-
-        //then
-        boolean userIsLoaded = emf.getPersistenceUnitUtil().isLoaded(findComment.getUser());
-        boolean reviewIsLoaded = emf.getPersistenceUnitUtil().isLoaded(findComment.getReview());
-        assertThat(userIsLoaded).isTrue();
-        assertThat(reviewIsLoaded).isTrue();
-        assertThat(savedComment.getContent()).isEqualTo(comment.getContent());
-        assertThat(savedComment.getUser().getNickname()).isEqualTo(user.getNickname());
-        assertThat(savedComment.getReview().getContent()).isEqualTo(review.getContent());
-    }
 
     @Test
+    @DisplayName("DTO 프로젝션 성공")
     void findCommentDto() {
         //given
         User user = User.create("test@gmail", "test", "12345678q!");
@@ -131,51 +144,100 @@ public class CommentRepositoryTest {
         assertThat(commentDto.getUserId()).isEqualTo(savedUser.getId());
     }
 
-    @Test
-    void searchComments() {
-        //given
-        User user = User.create("test@gmail", "test", "12345678q!");
-        User savedUser = userRepository.save(user);
-        Book book = Book.create(
-                "title", "content", "12345678",
-                LocalDate.now(), "publisher",
-                "thumbnailUrl", "description");
-        Book savedBook = bookRepository.save(book);
-        Review review = Review.create(5.0, "content", savedBook, savedUser);
-        Review savedReview = reviewRepository.save(review);
-        List<Comment> commentList = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            Comment comment = Comment.create("content" + i, savedUser, savedReview);
-            commentList.add(comment);
+    @Nested
+    @DisplayName("댓글 목록 커서 페이지네이션")
+    class Search {
+        @Test
+        @DisplayName("댓글 목록 조회 성공 - 내림차순")
+        void searchComments_no_cursor_desc() {
+            //given
+            User user = User.create("test@gmail", "test", "12345678q!");
+            User savedUser = userRepository.save(user);
+            Book book = Book.create(
+                    "title", "content", "12345678",
+                    LocalDate.now(), "publisher",
+                    "thumbnailUrl", "description");
+            Book savedBook = bookRepository.save(book);
+            Review review = Review.create(5.0, "content", savedBook, savedUser);
+            Review savedReview = reviewRepository.save(review);
+            List<Comment> commentList = new ArrayList<>();
+            for (int i = 0; i < 100; i++) {
+                Comment comment = Comment.create("content" + i, savedUser, savedReview);
+                commentList.add(comment);
+            }
+            commentRepository.saveAll(commentList);
+            em.flush();
+            em.clear();
+
+            CommentSearchCondition condition = new CommentSearchCondition(
+                    savedReview.getId(),
+                    "DESC",
+                    null,
+                    null,
+                    20);
+
+            //when
+            List<Comment> results = commentRepository.searchComments(condition);
+
+
+            //then
+            assertThat(results.size()).isEqualTo(condition.limit());
+            assertThat(results.get(0).getCreatedAt())
+                    .isAfterOrEqualTo(results.get(results.size() - 1).getCreatedAt());
+            assertThat(results).extracting(Comment::getCreatedAt).isSortedAccordingTo(Comparator.reverseOrder());
+            assertThat(results).allSatisfy(comment -> {
+                assertThat(comment.getReview().getId()).isEqualTo(savedReview.getId());
+            });
         }
-        commentRepository.saveAll(commentList);
-        em.flush();
-        em.clear();
 
-        CommentSearchCondition condition = new CommentSearchCondition(
-                savedReview.getId(),
-                "DESC",
-                null,
-//                commentList.get(75).getCreatedAt().toString(),
-//                commentList.get(75).getCreatedAt(),
-                null,
-                20);
+        @Test
+        @DisplayName("댓글 목록 조회 성공- after값 기준 내림차순")
+        void searchComments_after_desc() {
+            //given
+            User user = User.create("test@gmail", "test", "12345678q!");
+            User savedUser = userRepository.save(user);
+            Book book = Book.create(
+                    "title", "content", "12345678",
+                    LocalDate.now(), "publisher",
+                    "thumbnailUrl", "description");
+            Book savedBook = bookRepository.save(book);
+            Review review = Review.create(5.0, "content", savedBook, savedUser);
+            Review savedReview = reviewRepository.save(review);
+            List<Comment> commentList = new ArrayList<>();
+            for (int i = 0; i < 100; i++) {
+                Comment comment = Comment.create("content" + i, savedUser, savedReview);
+                commentList.add(comment);
+            }
+            commentRepository.saveAll(commentList);
+            em.flush();
+            em.clear();
 
-        //when
-        List<Comment> results = commentRepository.searchComments(condition);
+            CommentSearchCondition condition = new CommentSearchCondition(
+                    savedReview.getId(),
+                    "DESC",
+                    commentList.get(75).getCreatedAt().toString(),
+                    commentList.get(75).getCreatedAt(),
+                    20);
 
-        for (Comment result : results) {
-            System.out.println("result = " + result.getContent());
-            System.out.println("result = " + result.getCreatedAt());
+            //when
+            List<Comment> results = commentRepository.searchComments(condition);
+
+            for (Comment result : results) {
+                System.out.println("result = " + result.getContent());
+                System.out.println("result = " + result.getCreatedAt());
+            }
+
+            //then
+            assertThat(results.size()).isEqualTo(condition.limit());
+            assertThat(results.get(0).getCreatedAt())
+                    .isAfterOrEqualTo(results.get(results.size() - 1).getCreatedAt());
+            assertThat(results).extracting(Comment::getCreatedAt).isSortedAccordingTo(Comparator.reverseOrder());
+            assertThat(results).allSatisfy(comment -> {
+                assertThat(comment.getReview().getId()).isEqualTo(savedReview.getId());
+            });
+            assertThat(results).allSatisfy(c ->
+                    assertThat(c.getCreatedAt()).isBefore(commentList.get(75).getCreatedAt())
+            );
         }
-
-        //then
-        assertThat(results.size()).isEqualTo(condition.limit());
-        assertThat(results.get(0).getCreatedAt())
-                .isAfterOrEqualTo(results.get(results.size() - 1).getCreatedAt());
-        assertThat(results).extracting(Comment::getCreatedAt).isSortedAccordingTo(Comparator.reverseOrder());
-        assertThat(results).allSatisfy(comment -> {
-            assertThat(comment.getReview().getId()).isEqualTo(savedReview.getId());
-        });
     }
 }
