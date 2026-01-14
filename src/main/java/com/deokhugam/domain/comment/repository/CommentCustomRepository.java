b88fd64b3e966517032fd291d452e6ee86fcda63
ian <dldks1290@gmail.com>
2026-01-14 09:10:16 +0900

    feat: reviewId를 통한 댓글 개수 구하는 getCountByReviewId() 추가

--- src/main/java/com/deokhugam/domain/comment/repository/CommentCustomRepository.java
+++ src/main/java/com/deokhugam/domain/comment/repository/CommentCustomRepository.java
@@ -1,10 +1,8 @@
 package com.deokhugam.domain.comment.repository;
 
 import com.deokhugam.domain.comment.dto.request.CommentSearchCondition;
-import com.deokhugam.domain.comment.dto.response.CommentDto;
 import com.deokhugam.domain.comment.entity.Comment;
 
-import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -13,5 +11,9 @@ public interface CommentCustomRepository {
 
     List<Comment> searchComments(CommentSearchCondition condition);
 
-    Optional<CommentDto> findCommentDto(UUID commentId);
+    Optional<Comment> findWithUser(UUID commentId);
+
+    Optional<Comment> findWithUserAndReview(UUID commentId);
+
+    long getCountByReviewId(UUID reviewId);
 }
