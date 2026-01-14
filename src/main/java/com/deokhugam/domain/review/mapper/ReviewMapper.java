b7891c80bc98924e00958182c9cd1bf531b7cbc1
hilee <eldh2002@gmail.com>
2026-01-14 19:03:40 +0900

    test

--- src/main/java/com/deokhugam/domain/review/mapper/ReviewMapper.java
+++ src/main/java/com/deokhugam/domain/review/mapper/ReviewMapper.java
@@ -6,21 +6,21 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ReviewMapper {
-//    public ReviewDto toReviewDto(Review review, long commentCount, boolean likedByMe) {
-//        return new ReviewDto(
-//                review.getId(),
-//                review.getUser().getId(),
-//                review.getBook().getId(),
-//                review.getBook().getTitle(),
-//                review.getBook().getThumbnailUrl(),
-//                review.getRating(),
-//                review.getUser().getNickname(),
-//                review.getContent(),
-//                review.getLikedCount(),
-//                commentCount,
-//                likedByMe,
-//                review.getCreatedAt(),
-//                review.getUpdatedAt()
-//        );
-//    }
+    public ReviewDto toReviewDto(Review review, long commentCount, boolean likedByMe) {
+        return new ReviewDto(
+                review.getId(),
+                review.getUser().getId(),
+                review.getBook().getId(),
+                review.getBook().getTitle(),
+                review.getBook().getThumbnailUrl(),
+                review.getRating(),
+                review.getUser().getNickname(),
+                review.getContent(),
+                review.getLikedCount(),
+                commentCount,
+                likedByMe,
+                review.getCreatedAt(),
+                review.getUpdatedAt()
+        );
+    }
 }
