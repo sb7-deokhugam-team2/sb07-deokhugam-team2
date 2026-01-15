58777dd1fe0d2d719cbfd68b6377d60a5ebeacd7
taeeonkim <myhom43@gmail.com>
2026-01-16 01:45:26 +0900

    feat: 도서 논리 및 물리 삭제 기능 추가
    - softDeleteBook: 논리 삭제 시 isDeleted 필드를 true로 변경
    - hardDeleteBook: 물리 삭제 시 존재하지 않는 Book ID에 대해 예외 처리 추가
    - 관련 통합 테스트 및 단위 테스트 케이스 작성 및 검증

--- src/test/java/com/deokhugam/deokhugam/book/integration/service/BookServiceIntegration.java
+++ src/test/java/com/deokhugam/deokhugam/book/integration/service/BookServiceIntegration.java
@@ -6,6 +6,7 @@ import com.deokhugam.domain.book.dto.response.CursorPageResponseBookDto;
 import com.deokhugam.domain.book.entity.Book;
 import com.deokhugam.domain.book.enums.SortCriteria;
 import com.deokhugam.domain.book.enums.SortDirection;
+import com.deokhugam.domain.book.exception.BookNotFoundException;
 import com.deokhugam.domain.book.repository.BookRepository;
 import com.deokhugam.domain.book.service.BookService;
 import jakarta.persistence.EntityManager;
@@ -18,9 +19,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 import static java.lang.Thread.sleep;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @Transactional
@@ -69,6 +72,72 @@ public class BookServiceIntegration {
             assertThat(lastDto.title()).isEqualTo(cursorPageResponseBookDto.nextCursor());
             assertThat(lastDto.createdAt()).isEqualTo(cursorPageResponseBookDto.nextAfter());
 
+        }
+    }
+
+    @Nested
+    @DisplayName("도서 논리삭제")
+    class SoftDelete {
+        @Test
+        @DisplayName("[Positive] 도서 논리 삭제 - 논리 삭제시 isDeleted가 true 변경")
+        void softDeleteBook_should_change_isDeleted_true() {
+            // given
+            Book book = Book.create("title", "author", "isbn", LocalDate.now(), "publisher", null, "desc");
+            bookRepository.save(book);
+            UUID bookId = book.getId();
+            em.flush();
+            em.clear();
+
+            // when
+            bookService.softDeleteBook(bookId);
+            em.flush();
+            em.clear();
+            Book foundBook = bookRepository.findById(bookId).orElseThrow();
+            // then
+            assertThat(foundBook.isDeleted()).isTrue();
+        }
+
+        @Test
+        @DisplayName("[Propagation][Negative] 도서 논리 삭제 -  해당 book id의 book이 DB에하는게 없을 시 BookNotFoundException 예외전파")
+        void softDeleteBook_should_throw_BookNotFoundException_when_notFound() {
+            // given
+            UUID invalidBookId = UUID.randomUUID();
+
+            // when & then
+            assertThrows(BookNotFoundException.class, () -> bookService.softDeleteBook(invalidBookId));
+        }
+    }
+
+    @Nested
+    @DisplayName("도서 물리삭제")
+    class HardDelete {
+        @Test
+        @DisplayName("[Positive] 도서 물리 삭제 - 삭제 후 findById는 empty")
+        void hardDeleteBook_should_return_empty_book() {
+            // given
+            Book book = Book.create("title", "author", "isbn", LocalDate.now(), "publisher", null, "desc");
+            bookRepository.save(book);
+            UUID bookId = book.getId();
+            em.flush();
+            em.clear();
+            assertThat(bookRepository.findById(bookId)).isPresent(); // 가정 명시
+            // when
+            bookService.hardDeleteBook(bookId);
+            em.flush();
+            em.clear();
+
+            // then
+            assertThat(bookRepository.findById(bookId)).isEmpty();
+        }
+
+        @Test
+        @DisplayName("[Negative] 물리 삭제 - 해당 book id 없을시 BookNotFound 예외전파")
+        void hard_delete_should_throw_BookNotFoundException_when_notFound() {
+            // given
+            UUID invalidBookId = UUID.randomUUID();
+
+            // when & then
+            assertThrows(BookNotFoundException.class, () -> bookService.hardDeleteBook(invalidBookId));
         }
     }
 
