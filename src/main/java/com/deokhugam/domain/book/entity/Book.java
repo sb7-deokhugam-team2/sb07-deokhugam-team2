7639d36f59eec10a2610bb50a589fca98e36f8df
JoSungYeon-000607 <zounsam00@gmail.com>
2026-01-15 13:04:40 +0900

    chore : BookEntity 잘못 Import되어있던 내용 수정

--- src/main/java/com/deokhugam/domain/book/entity/Book.java
+++ src/main/java/com/deokhugam/domain/book/entity/Book.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.*;
-import software.amazon.awssdk.annotations.ThreadSafe;
 
 import java.time.LocalDate;
 
