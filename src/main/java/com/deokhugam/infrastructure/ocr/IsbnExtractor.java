e186802a1ad223ce0c563979cb725d2303788363
JoSungYeon-000607 <zounsam00@gmail.com>
2026-01-16 17:13:19 +0900

    Revert "feat : OCRApi기능 구현, 에로 코드 작성"
    This reverts commit 7f5735039e288d07d600f66aef40a5fde108b630.

--- src/main/java/com/deokhugam/infrastructure/ocr/IsbnExtractor.java
+++
@@ -1,7 +0,0 @@
-package com.deokhugam.infrastructure.ocr;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface IsbnExtractor {
-    String extractIsbn(MultipartFile image);
-}
