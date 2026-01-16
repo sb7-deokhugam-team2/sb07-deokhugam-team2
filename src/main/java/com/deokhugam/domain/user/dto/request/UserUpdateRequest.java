9e47826dfa1ff3750cb9c5ac3a7d3d7cd5a1079a
yumi Kim <cdcd0214@gmail.com>
2026-01-16 11:14:07 +0900

    fix : CommentController Header에 -User-추가, NotificationController에 RequestMapping 작성, User Validation 예외 수정, UserUpdateRequest 문자열에 @Json 추가

--- src/main/java/com/deokhugam/domain/user/dto/request/UserUpdateRequest.java
+++ src/main/java/com/deokhugam/domain/user/dto/request/UserUpdateRequest.java
@@ -1,10 +1,18 @@
 package com.deokhugam.domain.user.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserUpdateRequest(
         @Size(min = 2, max = 20)
         @NotBlank
+        @JsonValue
         String nickname
-) {}
+) {
+    @JsonCreator
+    public UserUpdateRequest(String nickname) {
+        this.nickname = nickname;
+    }
+}
