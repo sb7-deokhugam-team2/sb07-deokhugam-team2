a8498a6a83d0d3677a3974add634c9644c95b49a
yumi Kim <cdcd0214@gmail.com>
2026-01-16 16:18:40 +0900

    feature : Notification의 커서 기반 페이지네이션 로직 작성(CustomRepository, Service, Controller)

--- src/main/java/com/deokhugam/domain/notification/repository/NotificationCustomRepositoryImpl.java
+++ src/main/java/com/deokhugam/domain/notification/repository/NotificationCustomRepositoryImpl.java
@@ -2,16 +2,61 @@ package com.deokhugam.domain.notification.repository;
 
 import com.deokhugam.domain.notification.dto.request.NotificationSearchCondition;
 import com.deokhugam.domain.notification.entity.Notification;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
+import java.time.Instant;
 import java.util.List;
+
+import static com.deokhugam.domain.notification.entity.QNotification.notification;
+import static com.deokhugam.domain.review.entity.QReview.review;
+import static com.deokhugam.domain.user.entity.QUser.user;
 
 @RequiredArgsConstructor
 public class NotificationCustomRepositoryImpl implements NotificationCustomRepository{
-    private JPAQueryFactory jpaQueryFactory;
+
+    private final JPAQueryFactory jpaQueryFactory;
+
     @Override
     public List<Notification> searchNotifications(NotificationSearchCondition condition) {
-        return List.of();
+        return jpaQueryFactory.select(notification)
+                .from(notification)
+                .join(notification.review, review).fetchJoin()
+                .join(notification.user, user).fetchJoin()
+                .where(
+                        notification.user.id.eq(condition.userId()),
+                        notification.confirmed.isTrue(),
+                        cursorCondition(condition.cursor(), condition.direction()),
+                        afterCondition(condition.after(), condition.direction())
+                )
+                .orderBy(direction(condition.direction()))
+                .limit(condition.limit()+1)
+                .fetch();
+    }
+
+    private OrderSpecifier<?>[] direction(String direction) {
+        if(direction.equals("ASC")){
+            return new OrderSpecifier[]{notification.createdAt.asc()};
+        }
+        return new OrderSpecifier[]{notification.createdAt.desc()};
+    }
+
+    private BooleanExpression cursorCondition(String cursor, String direction) {
+        if(cursor == null){
+            return null;
+        }
+        Instant cursorInstant = Instant.parse(cursor);
+        if(direction.equals("ASC")) return notification.createdAt.gt(cursorInstant);
+        return notification.createdAt.lt(cursorInstant);
+    }
+
+    private BooleanExpression afterCondition(Instant after, String direction) {
+        if(after == null){
+            return null;
+        }
+        if(direction.equals("ASC")) return notification.createdAt.gt(after);
+        return notification.createdAt.lt(after);
     }
 }
--- src/main/java/com/deokhugam/domain/notification/service/NotificationServiceImpl.java
+++ src/main/java/com/deokhugam/domain/notification/service/NotificationServiceImpl.java
@@ -4,14 +4,22 @@ import com.deokhugam.domain.notification.dto.request.NotificationSearchCondition
 import com.deokhugam.domain.notification.dto.request.NotificationUpdateRequest;
 import com.deokhugam.domain.notification.dto.response.CursorPageResponseNotificationDto;
 import com.deokhugam.domain.notification.dto.response.NotificationDto;
+import com.deokhugam.domain.notification.repository.NotificationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class NotificationServiceImpl implements NotificationService {
+
+    private final NotificationRepository notificationRepository;
 
     @Override
     public NotificationDto readNotification(UUID notificationId, UUID userId, NotificationUpdateRequest notificationUpdateRequest) {
@@ -24,8 +32,37 @@ public class NotificationServiceImpl implements NotificationService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public CursorPageResponseNotificationDto getNotifications(NotificationSearchCondition condition) {
-        return null;
+        List<NotificationDto> notifications = notificationRepository.searchNotifications(condition)
+                .stream()
+                .map(NotificationDto::from)
+                .collect(Collectors.toList());
+
+        boolean hasNext = notifications.size() > condition.limit();
+        if(notifications.size() > condition.limit()){
+            notifications.remove(notifications.size() - 1);
+        }
+
+        String nextCursor = null;
+        Instant nextAfter = null;
+
+        if(!notifications.isEmpty()){
+            NotificationDto lastItem = notifications.get(notifications.size() - 1);
+            nextCursor = lastItem.getCreatedAt().toString();
+            nextAfter = lastItem.getCreatedAt();
+        }
+
+        long totalElements = notificationRepository.count();
+
+        return CursorPageResponseNotificationDto.from(
+                notifications,
+                nextCursor,
+                nextAfter,
+                notifications.size(),
+                totalElements,
+                hasNext
+        );
     }
 
     @Override
