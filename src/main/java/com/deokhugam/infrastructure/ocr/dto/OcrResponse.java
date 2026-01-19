87cc7f6a7804aa88060ace1e45500cb19f0de0a1
JoSungYeon-000607 <zounsam00@gmail.com>
2026-01-19 09:06:13 +0900

    feat : OCR기본 기능틀 추가

---
+++ src/main/java/com/deokhugam/infrastructure/ocr/dto/OcrResponse.java
@@ -0,0 +1,16 @@
+package com.deokhugam.infrastructure.ocr.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public record OcrResponse(
+        @JsonProperty("ParsedResults") List<ParsedResult> parsedResults,
+        @JsonProperty("ErrorMessage") String errorMessage, // API 에러 메시지 필드 추가
+        @JsonProperty("IsErroredOnProcessing") boolean isErrored
+) {
+    record ParsedResult(
+            @JsonProperty("ParsedText") String parsedText,
+            @JsonProperty("ErrorMessage") String errorMessage
+    ){}
+}
