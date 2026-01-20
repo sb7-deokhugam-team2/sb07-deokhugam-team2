100cba182192b88b814044b1b6862842aa3f3808
JoSungYeon-000607 <zounsam00@gmail.com>
2026-01-20 10:37:23 +0900

    chore : 스케줄러 활성화 및 yml 일부 수정

--- src/main/java/com/deokhugam/DeokhugamApplication.java
+++ src/main/java/com/deokhugam/DeokhugamApplication.java
@@ -2,10 +2,12 @@ package com.deokhugam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableScheduling
 public class DeokhugamApplication {
 
     public static void main(String[] args) {
