7db193522b99371f8f95e2fa98e8132c83c489f2
ian <dldks1290@gmail.com>
2026-01-20 16:21:23 +0900

    feat:  PowerUser 컨트롤러 작성과 배치를 동작시키기 위한 api 임의 작성

--- src/main/java/com/deokhugam/domain/poweruser/controller/PowerUserController.java
+++ src/main/java/com/deokhugam/domain/poweruser/controller/PowerUserController.java
@@ -1,12 +1,20 @@
 package com.deokhugam.domain.poweruser.controller;
 
+import com.deokhugam.domain.base.PeriodType;
 import com.deokhugam.domain.poweruser.dto.request.PowerUserSearchCondition;
+import com.deokhugam.domain.poweruser.dto.response.CursorPageResponsePowerUserDto;
 import com.deokhugam.domain.poweruser.service.PowerUserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,8 +23,19 @@ public class PowerUserController {
     private final PowerUserService powerUserService;
 
     @GetMapping("/api/users/power")
-    public void getPowerUsers(
+    public ResponseEntity<CursorPageResponsePowerUserDto> getPowerUsers(
             @Valid @ModelAttribute PowerUserSearchCondition condition){
-//        powerUserService
+        CursorPageResponsePowerUserDto powerUsers = powerUserService.findPowerUsers(condition);
+        return ResponseEntity.ok().body(powerUsers);
+    }
+
+    @GetMapping("/api/users/batch")
+    public void start(){
+        ZonedDateTime zonedDateTime = ZonedDateTime.now(ZoneId.systemDefault())
+                .truncatedTo(ChronoUnit.DAYS);
+        List<PeriodType> periodTypeList = List.of(PeriodType.values());
+        for (PeriodType periodType : periodTypeList) {
+            powerUserService.calculateRankingByPeriod(periodType, zonedDateTime);
+        }
     }
 }
