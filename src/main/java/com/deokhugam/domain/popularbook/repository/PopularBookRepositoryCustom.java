419f9250e7bd05776b66374b5f929c2adcd6b769
taeeonkim <myhom43@gmail.com>
2026-01-21 08:18:40 +0900

    feat: 인기 도서 조회를 위한 새로운 쿼리 메서드 추가
    - PopularBookRepository: 최신 계산된 날짜 기준 인기 도서 정보를 조회하는 메서드(`findTopPopularBooks`) 구현
    - PopularBookRepositoryCustom: 관련 메서드 시그니처 추가
    - 인기 도서 데이터 조회 로직 확장을 통해 다양한 기간 조회 지원

--- src/main/java/com/deokhugam/domain/popularbook/repository/PopularBookRepositoryCustom.java
+++ src/main/java/com/deokhugam/domain/popularbook/repository/PopularBookRepositoryCustom.java
@@ -1,12 +1,15 @@
 package com.deokhugam.domain.popularbook.repository;
 
 import com.deokhugam.domain.base.PeriodType;
+import com.deokhugam.domain.book.dto.response.PopularBookDto;
 import com.deokhugam.domain.popularbook.dto.response.PopularBookAggregationDto;
 
+import java.time.Instant;
 import java.util.List;
 
 public interface PopularBookRepositoryCustom {
     List<PopularBookAggregationDto> findTopPopularBookAggregates(PeriodType periodType, int limit);
+    List<PopularBookDto> findTopPopularBooks(PeriodType periodType, int limit, Instant windowStart);
 
 
 
--- src/main/java/com/deokhugam/domain/popularbook/repository/PopularBookRepositoryImpl.java
+++ src/main/java/com/deokhugam/domain/popularbook/repository/PopularBookRepositoryImpl.java
@@ -1,9 +1,12 @@
 package com.deokhugam.domain.popularbook.repository;
 
 import com.deokhugam.domain.base.PeriodType;
+import com.deokhugam.domain.book.dto.response.PopularBookDto;
 import com.deokhugam.domain.popularbook.dto.response.PopularBookAggregationDto;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.NumberExpression;
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
@@ -12,6 +15,7 @@ import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 import static com.deokhugam.domain.book.entity.QBook.book;
+import static com.deokhugam.domain.popularbook.entity.QPopularBook.popularBook;
 import static com.deokhugam.domain.review.entity.QReview.review;
 
 @RequiredArgsConstructor
@@ -54,6 +58,43 @@ public class PopularBookRepositoryImpl implements PopularBookRepositoryCustom {
                         book.createdAt.desc(),
                         book.id.asc()
                 )
+                .limit(limit)
+                .fetch();
+    }
+
+    @Override
+    public List<PopularBookDto> findTopPopularBooks(PeriodType periodType, int limit, Instant windowStart) {
+        JPQLQuery<Instant> latestCalculatedDateSubquery =
+                JPAExpressions
+                        .select(popularBook.calculatedDate.max())
+                        .from(popularBook)
+                        .where(
+                                popularBook.periodType.eq(periodType), // 기간타입이 같고
+                                popularBook.calculatedDate.goe(windowStart) // 윈도우시작보다 큰(원하는 범위의 최신 행들만)값인 경우
+                        );
+        return queryFactory
+                .select(Projections.constructor(
+                                PopularBookDto.class,
+                                popularBook.id,
+                                book.id,
+                                book.title,
+                                book.author,
+                                book.thumbnailUrl,
+                                popularBook.periodType,
+                                popularBook.rank,
+                                popularBook.score,
+                                popularBook.reviewCount,
+                                popularBook.rating,
+                                popularBook.createdAt
+                        )
+                )
+                .from(popularBook)
+                .join(popularBook.book, book)
+                .where(
+                        popularBook.periodType.eq(periodType),
+                        popularBook.calculatedDate.eq(latestCalculatedDateSubquery)
+                )
+                .orderBy(popularBook.rank.asc())
                 .limit(limit)
                 .fetch();
     }
