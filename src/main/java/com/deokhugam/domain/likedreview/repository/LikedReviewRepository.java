f57c1c3a9d9a09e543f3c9f2f138067816d3820a
hilee <eldh2002@gmail.com>
2026-01-21 15:33:20 +0900

    add: LikedReview 로직 작성

--- src/main/java/com/deokhugam/domain/likedreview/repository/LikedReviewRepository.java
+++ src/main/java/com/deokhugam/domain/likedreview/repository/LikedReviewRepository.java
@@ -3,7 +3,9 @@ package com.deokhugam.domain.likedreview.repository;
 import com.deokhugam.domain.likedreview.entity.LikedReview;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface LikedReviewRepository extends JpaRepository<LikedReview, UUID> {
+    Optional<LikedReview> findByReviewIdAndUserId(UUID reviewId, UUID userId);
 }
