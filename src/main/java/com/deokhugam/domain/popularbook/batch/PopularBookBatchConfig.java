62de02ee980d10cddf52f03db77a60d8616e97d5
taeeonkim <myhom43@gmail.com>
2026-01-23 01:38:56 +0900

    feat: 인기 도서 스냅샷 배치 및 스케줄러 로직 추가
    - Spring Batch를 활용한 인기 도서 스냅샷 배치 작업 구성 추가
    - 스냅샷 생성 배치와 연동된 스케줄러 로직 리팩토링
    - 기간 타입에 따른 스냅샷 생성 처리 로직 구현
    - JobLauncher 및 JobParametersBuilder 활용하여 동적 배치 실행 지원

---
+++ src/main/java/com/deokhugam/domain/popularbook/batch/PopularBookBatchConfig.java
@@ -0,0 +1,47 @@
+package com.deokhugam.domain.popularbook.batch;
+
+import com.deokhugam.domain.base.PeriodType;
+import com.deokhugam.domain.popularbook.service.PopularBookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.util.Map;
+
+@Configuration
+@RequiredArgsConstructor
+public class PopularBookBatchConfig {
+
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
+    private final PopularBookService popularBookService;
+
+    @Bean
+    public Job popularBookSnapshotJop(Step popularBookSnapshotStep) {
+        return new JobBuilder("popularBookSnapshotJob", jobRepository)
+                .start(popularBookSnapshotStep)
+                .build();
+    }
+
+    @Bean
+    public Step popularBookSnapshotStep() {
+        return new StepBuilder("popularBookSnapshotJop", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    Map<String, Object> jobParameters = chunkContext.getStepContext().getJobParameters();
+                    String period = (String) jobParameters.get("periodType");
+                    PeriodType periodType = PeriodType.valueOf(period);
+                    popularBookService.snapshotPopularBooks(periodType);
+                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+    }
+
+
+}
--- src/main/java/com/deokhugam/domain/popularbook/scheduler/PopularBookScheduler.java
+++ src/main/java/com/deokhugam/domain/popularbook/scheduler/PopularBookScheduler.java
@@ -1,8 +1,11 @@
 package com.deokhugam.domain.popularbook.scheduler;
 
 import com.deokhugam.domain.base.PeriodType;
-import com.deokhugam.domain.popularbook.service.PopularBookService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -10,27 +13,38 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class PopularBookScheduler {
 
-    private final PopularBookService popularBookService;
+    private final JobLauncher jobLauncher;
+    private final Job popularBookSnapshotJob;
 
     // NOTE: 주의 - popularBookQueryWindowCalculator 값과 일치하게 연산하여 스냅샷 남기도록해야 조회시 불일치 없음
     @Scheduled(cron = "0 */30 * * * *") // 30분마다(interver)
-    public void runDailyRanking() {
-        popularBookService.snapshotPopularBooks(PeriodType.DAILY);
+    public void runDailyRanking() throws Exception {
+
+        runJob(PeriodType.DAILY);
     }
 
     @Scheduled(cron = "0 */30 * * * *")
-    public void runWeeklyRanking() {
-        popularBookService.snapshotPopularBooks(PeriodType.WEEKLY);
+    public void runWeeklyRanking() throws Exception {
+        runJob(PeriodType.WEEKLY);
     }
 
     @Scheduled(cron = "0 */30 * * * *")
-    public void runMonthlyRanking() {
-        popularBookService.snapshotPopularBooks(PeriodType.MONTHLY);
+    public void runMonthlyRanking() throws Exception {
+        runJob(PeriodType.MONTHLY);
     }
 
     @Scheduled(cron = "0 */30 * * * *")
-    public void runAllRanking() {
-        popularBookService.snapshotPopularBooks(PeriodType.ALL_TIME);
+    public void runAllRanking() throws Exception {
+        runJob(PeriodType.ALL_TIME);
+    }
+
+    private void runJob(PeriodType periodType) throws Exception {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("periodType", periodType.name())
+                .toJobParameters();
+
+        jobLauncher.run(popularBookSnapshotJob, jobParameters);
+
     }
 
 }
