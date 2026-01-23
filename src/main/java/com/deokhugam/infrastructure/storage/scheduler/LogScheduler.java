933a5b3ca0c25c12e06dd5301ae09e11a4118ac2
JoSungYeon-000607 <zounsam00@gmail.com>
2026-01-23 16:55:57 +0900

    test : S3 LogStorage Scheduler및 Component 테스트 완료

--- src/main/java/com/deokhugam/infrastructure/storage/scheduler/LogScheduler.java
+++ src/main/java/com/deokhugam/infrastructure/storage/scheduler/LogScheduler.java
@@ -2,6 +2,7 @@ package com.deokhugam.infrastructure.storage.scheduler;
 
 import com.deokhugam.infrastructure.storage.LogS3Storage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -14,19 +15,20 @@ import java.util.Arrays;
 @RequiredArgsConstructor
 public class LogScheduler {
 
-    private final LogS3Storage logS3Storage; // 👈 새로 만든 클래스 주입
-    private static final String LOG_DIR = "./.logs";
+    private final LogS3Storage logS3Storage;
+    @Value("${log.path:./logs}")
+    private String logDir;
 
     @Scheduled(cron = "0 1 * * * *")
     public void uploadPastHourLog() {
-        File dir = new File(LOG_DIR);
+        File dir = new File(logDir);
 
         File[] logFiles = dir.listFiles((d, name) ->
                 name.startsWith("app-") && name.endsWith(".log") && !name.equals("app.log")
         );
 
         if (logFiles != null) {
-            Arrays.stream(logFiles).forEach(logS3Storage::uploadLog); // 👈 호출
+            Arrays.stream(logFiles).forEach(logS3Storage::uploadLog);
         }
     }
 }
