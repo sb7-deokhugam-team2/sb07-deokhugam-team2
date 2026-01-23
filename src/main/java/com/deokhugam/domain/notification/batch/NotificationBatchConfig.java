eafe42c9bd897462f14dd72de23386327d30b867
ian <dldks1290@gmail.com>
2026-01-23 13:46:17 +0900

    feat: notification

---
+++ src/main/java/com/deokhugam/domain/notification/batch/NotificationBatchConfig.java
@@ -0,0 +1,48 @@
+package com.deokhugam.domain.notification.batch;
+
+import com.deokhugam.domain.notification.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.time.Instant;
+import java.util.Map;
+
+@Configuration
+@RequiredArgsConstructor
+public class NotificationBatchConfig {
+
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
+    private final NotificationService notificationService;
+
+    @Bean
+    public Job notificationJob(Step notificationDelete) {
+        return new JobBuilder("notificationJob", jobRepository)
+                .start(notificationDelete)
+//                .listener(metricsListener)
+                .build();
+    }
+
+    @Bean
+    public Step notificationDelete() {
+        return new StepBuilder("notificationDelete", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    Map<String, Object> params = chunkContext.getStepContext().getJobParameters();
+
+                    String instantStr = (String) params.get("targetTime");
+                    Instant time = (instantStr != null) ? Instant.parse(instantStr) : Instant.now();
+                    notificationService.deleteNotifications(time);
+
+                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+    }
+}
--- src/main/java/com/deokhugam/domain/notification/scheduler/NotificationScheduler.java
+++ src/main/java/com/deokhugam/domain/notification/scheduler/NotificationScheduler.java
@@ -1,12 +1,12 @@
-package com.deokhugam.domain.notification.service;
+package com.deokhugam.domain.notification.scheduler;
 
+import com.deokhugam.domain.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
-import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
@@ -16,9 +16,9 @@ import java.time.temporal.ChronoUnit;
 @RequiredArgsConstructor
 public class NotificationScheduler {
 
-    private final NotificationService notificationService;
+    private final
 
-    @Scheduled(cron = "0 0 0 * * *", zone = "#{T(java.util.TimeZone).getDefault().getID()}")
+    @Scheduled(cron = "0 */30 * * * *", zone = "#{T(java.util.TimeZone).getDefault().getID()}")
     public void deleteNotifications(){
         log.info("[NotificationScheduler] start deleteNotifications {}", getClass());
         Instant midnight = ZonedDateTime.now(ZoneId.systemDefault())
